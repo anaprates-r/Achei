d549d778b934d2c14d4d820ce590fda6c5292da8
anaprates-r <ana.prates@ufms.br>
2026-04-18 16:15:49 -0400

    novo processamento

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,21 +1,20 @@
-from processamento import processar_r84
+from processamento_2 import limpeza_dos_dados
+# from processamento import processar_r84
 from models import Medicamento
 from config import db, app
 
 def etl(fileName):
     # Chama o processamento do arquivo e retorna um DataFrame limpo
-    df_limpo = processar_r84(fileName)
+    df_limpo = limpeza_dos_dados(fileName)
     
     with app.app_context():
-        # carrega tudo uma vez
-        existentes = {
-            (m.catmat, m.estabelecimento_saude): m
-            for m in Medicamento.query.all()
-        }
+        for _, row in df_limpo.iterrows():
+            # Busca se o par (catmat, estabelecimento) já existe
+            existente = Medicamento.query.filter_by(
+                catmat=row['catmat'], 
+                estabelecimento_saude=row['estabelecimento_saude']
+            ).first()
 
-        for row in df_limpo.to_dict(orient="records"):
-            chave = (row['catmat'], row['estabelecimento_saude'])
-            existente = existentes.get(chave)
             if existente:
                 # Atualiza a quantidade se já existir
                 existente.quantidade = row['quantidade']
