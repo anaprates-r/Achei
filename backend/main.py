a2a6c41ca51eff62bb748d11b2fb479a6830900c
anaprates-r <ana.prates@ufms.br>
2026-04-18 17:05:05 -0400

    rota upload

--- backend/main.py
+++ backend/main.py
@@ -49,7 +49,7 @@ def listar_estabelecimentos_unicas():
     # Retorna uma lista simples: ["UBS Centro", "Hospital Norte", ...]
     return jsonify([u[0] for u in estabelecimentos if u[0]])
 
-import threading
+#import threading
 
 def run_etl(file_path):
     try:
@@ -57,7 +57,7 @@ def run_etl(file_path):
         print("ETL finalizado com sucesso")
     except Exception as e:
         print(f"Erro no ETL: {e}")
-
+'''
 @app.route("/upload", methods=["POST"])
 def upload():
     if 'file' not in request.files:
@@ -76,10 +76,11 @@ def upload():
     #RODA EM BACKGROUND
     thread = threading.Thread(target=run_etl, args=(temp_path,))
     thread.start()
-
+    
     return jsonify({
         "message": "Arquivo enviado! Processamento em andamento..."
     }), 202
+    
 
 #To run the aplication:
 if __name__ == "__main__":
@@ -88,5 +89,44 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+'''
+@app.route("/upload", methods=["POST"])
+def upload():
+    # 1. Validação do arquivo
+    if 'file' not in request.files:
+        return jsonify({"message": "Nenhum arquivo enviado"}), 400
 
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"message": "Arquivo sem nome"}), 400
+
+    # 2. Caminhos (backend/uploads)
+    upload_path = os.path.join("uploads")
+    if not os.path.exists(upload_path):
+        os.makedirs(upload_path)
+
+    file_path = os.path.join(upload_path, file.filename)
+    file.save(file_path)
+
+    try:
+        # 3. O Pipeline aciona o Banco de Dados
+        # Passe o caminho completo do arquivo para o seu processador
+        etl(fileName=file_path) 
+
+        # 4. Retorno de sucesso para o React
+        # O React receberá esse 201 e saberá que os dados já estão no banco
+        return jsonify({
+            "message": "Arquivo processado e dados salvos no banco com sucesso!"
+        }), 201
+
+    except Exception as e:
+        # Se o banco de dados falhar, o erro cai aqui
+        print(f"Erro no pipeline: {e}")
+        return jsonify({"message": f"Erro ao salvar no banco: {str(e)}"}), 500
+    #To run the aplication:
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
 
