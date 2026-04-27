0eac906053be6862463c2265af924016b00974da
anaprates-r <ana.prates@ufms.br>
2026-04-27 11:09:57 -0400

    Simplify CORS configuration in config.py

--- backend/config.py
+++ backend/config.py
@@ -6,19 +6,7 @@ import os
 
 #__________INITIALISATIONS____________________
 app = Flask(__name__)
-CORS(
-    app,
-    resources={r"/*": {
-        "origins": "https://achei-medicamentos.vercel.app"
-    }},
-    supports_credentials=True
-)
-@app.after_request
-def apply_cors(response):
-    response.headers["Access-Control-Allow-Origin"] = "https://achei-medicamentos.vercel.app"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
-    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
-    return response
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
