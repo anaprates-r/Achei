abf07395d89995c840a499fc74d4ac0b26f2339e
anaprates-r <ana.prates@ufms.br>
2026-04-27 11:22:40 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,12 @@ from flask import request, jsonify
 from config import app,db
 from models import Medicamento
 from pipeline import etl
-import tempfile
 
 import os
 
-
+@app.route('/')
+def index_page():
+    return "<h1> Flask API </h1>"
 @app.route('/medicamentos', methods=['GET'])
 def listar_medicamentos():
     # 1. Captura parâmetros de paginação
@@ -49,52 +50,8 @@ def listar_estabelecimentos_unicas():
     # Retorna uma lista simples: ["UBS Centro", "Hospital Norte", ...]
     return jsonify([u[0] for u in estabelecimentos if u[0]])
 
-#import threading
-
-def run_etl(file_path):
-    try:
-        etl(fileName=file_path)
-        print("ETL finalizado com sucesso")
-    except Exception as e:
-        print(f"Erro no ETL: {e}")
-'''
 @app.route("/upload", methods=["POST"])
 def upload():
-    if 'file' not in request.files:
-        return jsonify({"message": "Nenhum arquivo enviado"}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"message": "Arquivo sem nome"}), 400
-
-    upload_path = "uploads"
-    os.makedirs(upload_path, exist_ok=True)
-
-    temp_path = os.path.join(upload_path, file.filename)
-    file.save(temp_path)
-
-    #RODA EM BACKGROUND
-    thread = threading.Thread(target=run_etl, args=(temp_path,))
-    thread.start()
-    
-    return jsonify({
-        "message": "Arquivo enviado! Processamento em andamento..."
-    }), 202
-    
-
-#To run the aplication:
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-'''
-@app.route("/upload", methods=["POST", "OPTIONS"])
-def upload():
-
-    if request.method == "OPTIONS":
-        return '', 200
     # 1. Validação do arquivo
     if 'file' not in request.files:
         return jsonify({"message": "Nenhum arquivo enviado"}), 400
@@ -104,7 +61,7 @@ def upload():
         return jsonify({"message": "Arquivo sem nome"}), 400
 
     # 2. Caminhos (backend/uploads)
-    upload_path = os.path.join("uploads")
+    upload_path = app.config['UPLOAD_FOLDER']
     if not os.path.exists(upload_path):
         os.makedirs(upload_path)
 
@@ -123,15 +80,15 @@ def upload():
         }), 201
 
     except Exception as e:
-        import traceback
-        traceback.print_exc()
         # Se o banco de dados falhar, o erro cai aqui
-        # print(f"Erro no pipeline: {e}")
+        print(f"Erro no pipeline: {e}")
         return jsonify({"message": f"Erro ao salvar no banco: {str(e)}"}), 500
-    #To run the aplication:
+
+
+
+#To run the aplication:
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
     app.run(debug=True)
-
