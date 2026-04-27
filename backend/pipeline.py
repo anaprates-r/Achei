1b0a1467d6664c16d6de4de8157247de3b4cdbee
anaprates-r <ana.prates@ufms.br>
2026-04-27 10:23:28 -0400

    Optimize ETL by using bulk_save_objects for inserts
    Refactor ETL process to use bulk_save_objects for new entries.

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,32 +1,33 @@
-from processamento_2 import limpeza_dos_dados
-# from processamento import processar_r84
-from models import Medicamento
-from config import db, app
-
 def etl(fileName):
-    # Chama o processamento do arquivo e retorna um DataFrame limpo
     df_limpo = limpeza_dos_dados(fileName)
-    
-    with app.app_context():
-        for _, row in df_limpo.iterrows():
-            # Busca se o par (catmat, estabelecimento) já existe
-            existente = Medicamento.query.filter_by(
-                catmat=row['catmat'], 
-                estabelecimento_saude=row['estabelecimento_saude']
-            ).first()
 
-            if existente:
-                # Atualiza a quantidade se já existir
+    with app.app_context():
+        # Carrega tudo do banco de uma vez
+        existentes = {
+            (m.catmat, m.estabelecimento_saude): m
+            for m in Medicamento.query.all()
+        }
+
+        novos = []
+
+        # sem query no banco
+        for _, row in df_limpo.iterrows():
+            chave = (row['catmat'], row['estabelecimento_saude'])
+
+            if chave in existentes:
+                existente = existentes[chave]
                 existente.quantidade = row['quantidade']
                 existente.medicamento = row['medicamento']
             else:
-                # Cria novo se não existir
                 novo = Medicamento(
                     catmat=row['catmat'],
                     medicamento=row['medicamento'],
                     quantidade=row['quantidade'],
                     estabelecimento_saude=row['estabelecimento_saude']
                 )
-                db.session.add(novo)
-        
+                novos.append(novo)
+
+        # 3. Insere tudo de uma vez
+        db.session.bulk_save_objects(novos)
+
         db.session.commit()
